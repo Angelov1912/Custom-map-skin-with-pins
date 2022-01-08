b73aa828473492ed001fcfe7cb847e5d83727af6
Stanimir Angelov <62446471+Angelov1912@users.noreply.github.com>
2022-01-08 00:01:21 +0000

    Update script.js
    Add some additional comments

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 //Coordinates- 37.994926, 23.697439
 //Coordinates are used to center the map to this location
 //In this case map is centered on the pin so coordinates are the same
-//Coordinates to be changed with clients location
+
+//IF USED AS A TEMPLATE, READ THE BELLOW COMMENTS
+
+
+//Map ID to be changed with ID of the relevant map
+//Coordinates to be changed with clients location on the relevant map
+//Path to marker images to be changed with the ones for the relvant map
 
 function initMap() {
-    //Location of the pin
+    //Location where we center the map
     const location = { lat: 37.994926, lng: 23.697439 };
-    //Map centered at the hospital
     const map = new google.maps.Map(document.getElementById("map"), {
       mapId: "a210e3b986403917",
       center: location,
@@ -15,20 +20,20 @@ function initMap() {
 
     //Add Marker
   const marker = new google.maps.Marker({
-    position: location,
+    position: location, //This map is centered on the pin, so we use the same variable
     map: map,
     title: "Location to be edited according to client",
     draggable: true,
     animation: google.maps.Animation.DROP, //Animation single drop effect on page load
     icon: {
       url: "hospital-h.svg",
-      scaledSize: new google.maps.Size(60, 60),
+      scaledSize: new google.maps.Size(60, 60), //Adjust the size of the marker
     }
   });
 
   //Create an info window for the location of the pin
   const infowindow = new google.maps.InfoWindow({
-    content: "Custom pin- text to be edited depending on the website",
+    content: "Custom pin- text to be edited depending on the website", //Text of the info bubble to be changed here
   });
 
   //Add event listeber that opens the info window when click on the pin
